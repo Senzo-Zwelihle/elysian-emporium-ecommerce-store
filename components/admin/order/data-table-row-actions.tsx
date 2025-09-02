0dc697dbb1cb6078fe423fe243168d3ce1cf41f6
Senzo-Zwelihle <senzozwelihlemasango@gmail.com>
2025-09-02 13:58:06 +0200

    orders page and components complete

---
+++ components/admin/order/data-table-row-actions.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import Link from "next/link";
+import { Row } from "@tanstack/react-table";
+import { MoreHorizontal } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { orderSchema } from "@/components/admin/order/data/schema";
+
+interface DataTableRowActionsProps<TData> {
+  row: Row<TData>;
+}
+
+export function DataTableRowActions<TData>({
+  row,
+}: DataTableRowActionsProps<TData>) {
+  const order = orderSchema.parse(row.original);
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="ghost"
+          size="icon"
+          className="data-[state=open]:bg-muted size-8"
+        >
+          <MoreHorizontal />
+          <span className="sr-only">Open menu</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-[160px]">
+        <DropdownMenuItem>
+          <Link href={`/admin/orders/${order.id}`}>View Details</Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem>Copy Order ID</DropdownMenuItem>
+
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <Link href={`/admin/orders/${order.id}/update`}>Edit Order</Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Link href={`/admin/orders/${order.id}/delete`}>Delete Order</Link>
+          <DropdownMenuShortcut>⌘⌫</DropdownMenuShortcut>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
